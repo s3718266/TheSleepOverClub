a8bf2eb1d5c5231d78768d53792d504ea585fbbc
Amy Thompson <37321347+s3718266@users.noreply.github.com>
2019-05-06 12:02:04 +1000

    Create productinfo.java

---
+++ productinfo.java
@@ -0,0 +1,34 @@
+package productInfo;
+
+public class ProductInfo {
+	public int productID;
+	public String productName;
+	public String productDescript;
+	public String location;
+	public String currentStock;
+	
+	
+	
+
+	public int getProductID() {
+		return this.productID;
+	}
+	
+	public String productName() {
+		return this.productName;
+	}
+	
+	public String productDescript() {
+		return this.productDescript;
+	}
+
+	public String location() {
+		return this.location;
+	}
+	
+	public String currentStock() {
+		return this.currentStock;
+	}
+	
+	
+}
